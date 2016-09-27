5f5160c168de951f2c0a0c954397670e6e5fbbf5
Whitney Murray <wnksmith92@yahoo.com>
2016-09-26 20:15:53 -0500

    job tips comp down

---
+++ scripts/components/AppComponent.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var JobTipsComponent = require('./JobTipsComponent.js');
+
+module.exports = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<JobTipsComponent / >
+				
+				
+			</div>
+		);
+	}
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
 'use strict';
-
 var React = require('react');
+var ReactDOM = require('react-dom');
+window.$ = require('jquery');
+window.jQuery = $;
 
+var AppComponent = require('./components/AppComponent');
+var JobTipsComponent = require('./components/JobTipsComponent');
+
+
+ReactDOM.render(
+	<JobTipsComponent />,
+	document.getElementById('app')
+	)
